14f8ccb28802f79011dc684aa0de8c68e7b1267e
zibillionbytes <f.alkan.93@outlook.com>
2024-05-18 16:22:41 +0300

    Created employee mapper and employee dto, mapped employee dto to employee.

---
+++ src/main/java/com/test/ems/mappers/EmployeeMapper.java
@@ -0,0 +1,23 @@
+package com.test.ems.mappers;
+
+import com.test.ems.dto.EmployeeDto;
+import com.test.ems.entity.Employee;
+
+public class EmployeeMapper {
+    public static EmployeeDto mapToEmployeeDto(Employee employee){
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+    }
+    public static Employee mapToEmployee(Employee employeeDto){
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+    }
+}
